71367d6e65aa9f562f3c35a1880a50fd723dcd5d
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-12 12:24:10 +0530

    changes in user interface

--- src/components/app/user/user.interface.ts
+++ src/components/app/user/user.interface.ts
@@ -1,12 +1,31 @@
 import mongoose from 'mongoose';
 
+interface IGiver {
+  check: boolean;
+  giverId: mongoose.Schema.Types.ObjectId;
+}
+
+interface ITaker {
+  check: boolean;
+  takerId: mongoose.Schema.Types.ObjectId;
+}
+type UserRoles = UserRole[]; // Array of roles
+
+type UserProps = {
+  [UserRole.Giver]: Giver;
+  [UserRole.Taker]: Taker;
+};
+
 export interface IUser {
   _id: mongoose.Schema.Types.ObjectId;
   username: String;
   email: String;
   password?: String;
   googleId: String;
-  isGiver: { check: Boolean; giverId: mongoose.Schema.Types.ObjectId };
-  isTaker: { check: Boolean; takerId: mongoose.Schema.Types.ObjectId };
+  isGiver?: { check: Boolean; giverId: mongoose.Schema.Types.ObjectId };
+  isTaker?: { check: Boolean; takerId: mongoose.Schema.Types.ObjectId };
+  role: UserRoles;
   phone?: String;
 }
+
+// to do add roles array and add mongoose functions to implement this
