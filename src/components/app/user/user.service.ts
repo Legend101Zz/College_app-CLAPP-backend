b8ea68e153176d68c3af485eb379840ccae8e2e9
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-15 01:40:04 +0530

    chnages in read service

--- src/components/app/user/user.service.ts
+++ src/components/app/user/user.service.ts
@@ -1,39 +1,35 @@
 import AppError from '@core/utils/appError';
 import logger from '@core/utils/logger';
-import mongoose from 'mongoose';
 import httpStatus from 'http-status';
 import { IUser } from './user.interface';
-
+import User from './user.model';
+import { assert } from 'console';
 // this service reads the user data and uses query to get specific data if needed
 
 type UserIncludeFields = Array<keyof IUser>;
-const read = (
+const read = async (
   userId: number,
   includeFields?: UserIncludeFields,
-): Partial<IUser> => {
+): Promise<Partial<IUser>> => {
   // Fetch user data from the database
-  let userData: IUser;
+  let query = User.findById(userId);
 
   if (includeFields && includeFields.length > 0) {
-    // Create a type that includes only the specified fields
-    type IncludedFields = Pick<IUser, typeof includeFields[number]>;
-
-    // Use Pick to create a type with the included fields
-    type PartialUserData = Partial<IncludedFields>;
-
-    // Create a partial user data object
-    const partialUserData: PartialUserData = includeFields.reduce(
-      (acc, key) => {
-        acc[key] = userData[key] as PartialUserData[keyof PartialUserData];
-        return acc;
-      },
-      {} as PartialUserData,
-    );
-
-    return partialUserData;
+    // If includeFields is provided, select only the specified fields
+    query = query.select(includeFields.join(' '));
   }
-  // Return an empty partial user data object if no fields are specified
-  return {};
+  // Execute the query
+  const userData = await query.exec();
+
+  if (!userData) {
+    // Handle the case where the user is not found
+    return {};
+  }
+
+  // Convert the Mongoose document to a plain JavaScript object
+  const userObject = userData.toObject();
+
+  return userObject as Partial<IUser>;
 };
 
 const update = (user: IUser): boolean => {
