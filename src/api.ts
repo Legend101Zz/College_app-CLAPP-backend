9625e6ec7181219daeb99a6eced7d8bffb919ce9
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-12-04 22:46:35 +0530

    minor changes

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import healthCheck from '@components/healthcheck/healthCheck.router';
-import user from '@components/dummy-for-ref/user.router';
+import user from '@components/app/user/user.router';
 
 const router: Router = Router();
 router.use(healthCheck);
