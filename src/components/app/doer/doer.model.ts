b4ff782c2af8eb9e6a7f96d24f72018eddb75c17
ice-009 <21bma009@nith.ac.in>
2023-12-08 10:55:47 +0530

    model of doer temp done

--- src/components/app/doer/doer.model.ts
+++ src/components/app/doer/doer.model.ts
@@ -2,5 +2,22 @@ import mongoose, { Schema } from 'mongoose';
 import { IDoer } from './doer.interface';
 
 const DoerSchema: Schema<IDoer> = new Schema({
-    
-})
+  ResponseTime: { type: Number },
+  Tasks: [
+    {
+      taskId: { type: mongoose.Schema.Types.ObjectId },
+      taskName: { type: String },
+    },
+  ],
+  Reviews: [
+    {
+      review: { type: String },
+      rating: { type: Number },
+    },
+  ],
+  Profile: { type: String },
+});
+
+const Doer = mongoose.model<IDoer>('Doer', DoerSchema);
+
+export default Doer;
