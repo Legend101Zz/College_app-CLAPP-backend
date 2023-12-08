dd3fe1f07b914584195a41f971faccf95525e04f
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-12-08 21:48:14 +0530

    added task schema and model

--- src/components/app/task/task.interface.ts
+++ src/components/app/task/task.interface.ts
@@ -1,11 +1,10 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import { Document } from 'mongoose';
 
 interface CommonTaskFields {
   title: string;
   description: string;
   category: 'content' | 'web' | 'video' | 'assignment' | 'design';
   deadline: Date;
-  budget: number;
 }
 
 interface AdditionalFields {
@@ -15,36 +14,3 @@ interface AdditionalFields {
 export interface Task extends CommonTaskFields, Document {
   additionalFields: AdditionalFields;
 }
-
-const taskSchema = new Schema<Task>({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    enum: ['content', 'web', 'video', 'assignment', 'design'],
-    required: true,
-  },
-  deadline: {
-    type: Date,
-    required: true,
-  },
-  budget: {
-    type: Number,
-    required: true,
-  },
-  additionalFields: {
-    type: Map,
-    of: String,
-    default: {},
-  },
-});
-
-const TaskModel = mongoose.model<Task>('Task', taskSchema);
-
-export default TaskModel;
