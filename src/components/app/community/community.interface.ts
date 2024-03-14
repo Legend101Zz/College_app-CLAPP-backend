775569cb37515b394c0da83d692b1e72f88c008d
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-14 23:57:29 +0530

    changes in manager schema

--- src/components/app/community/community.interface.ts
+++ src/components/app/community/community.interface.ts
@@ -1,19 +1,20 @@
 import mongoose, { Document, mongo } from 'mongoose';
 
 export interface IDoer {
-    doerId: mongoose.Schema.Types.ObjectId;
+  doerId: mongoose.Schema.Types.ObjectId;
 }
 
-export interface IRoom{
-    roomId: mongoose.Schema.Types.ObjectId;
+export interface IRoom {
+  roomId: mongoose.Schema.Types.ObjectId;
 }
 
 export interface ICommunity extends Document {
-    _id: mongoose.Schema.Types.ObjectId;
-    doer: IDoer[];
-    giver: mongoose.Schema.Types.ObjectId;
-    manager: mongoose.Schema.Types.ObjectId;
-    task: mongoose.Schema.Types.ObjectId;
-    rooms: IRoom[];
-    createdAt: Date;
+  _id: mongoose.Schema.Types.ObjectId;
+  name: String;
+  doer: IDoer[];
+  giver: mongoose.Schema.Types.ObjectId;
+  manager: mongoose.Schema.Types.ObjectId;
+  task: mongoose.Schema.Types.ObjectId;
+  rooms: IRoom[];
+  createdAt: Date;
 }
--- src/components/app/manager/manager.interface.ts
+++ src/components/app/manager/manager.interface.ts
@@ -1,40 +1,40 @@
 import mongoose, { Document } from 'mongoose';
 
 export interface ITasks {
-    taskId: mongoose.Schema.Types.ObjectId;
-    // taskName: string;
-  }
-  export interface IWallet {
-    walletId: mongoose.Schema.Types.ObjectId;
-    // walletName: string;
-  }  
-  export interface IReviews {
-    review: string;
-    rating: number;
-  }
-  export interface IEducation {
-    collegeName: string;
-    major: string;
-  }
-  
-  export interface IExperience {
-    companyName: string;
-    position: string;
-    startDate: Date;
-    endDate: Date;
-  }
-  export interface IManager extends mongoose.Document {
-    _id: mongoose.Schema.Types.ObjectId;
-    userId: mongoose.Schema.Types.ObjectId;
-    FullName: string;
-    DisName: string;
-    Description: string;
-    // Skills: ISkills[];
-    Education: IEducation;
-    Experience: IExperience;
-    LinkedIn: string;
-    walletId: IWallet;
-    Tasks: ITasks[];
-    Reviews: IReviews[];
-    Specialtags: string[];
-  }
+  taskId: mongoose.Schema.Types.ObjectId;
+  // taskName: string;
+}
+export interface IWallet {
+  walletId: mongoose.Schema.Types.ObjectId;
+  // walletName: string;
+}
+export interface IReviews {
+  review: string;
+  rating: number;
+}
+export interface IEducation {
+  collegeName: string;
+  major: string;
+}
+
+export interface IExperience {
+  companyName: string;
+  position: string;
+  startDate: Date;
+  endDate: Date;
+}
+export interface IManager extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId;
+  userId: mongoose.Schema.Types.ObjectId;
+  FullName: string;
+  DisName: string;
+  Description: string;
+  // Skills: ISkills[];
+  Education: IEducation;
+  Experience: IExperience;
+  LinkedIn: string;
+  walletId: IWallet;
+  Tasks: ITasks[];
+  Reviews: IReviews[];
+  Specialtags: string[];
+}
