00a4e55dea99c3724d532eec53543f9e6b7e8f67
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-16 22:33:23 +0530

    created a manager socket service

---
+++ src/components/app/manager/manager.socket.service.ts
@@ -0,0 +1,35 @@
+import { Server, Namespace } from 'socket.io';
+import logger from '@core/utils/logger';
+
+// Function to handle creation of namespaces
+const createNamespace = (
+  io: Server,
+  namespaceName: string,
+): Namespace | null => {
+  try {
+    // Create the namespace dynamically
+    const namespace = io.of(`/${namespaceName}`);
+
+    // Handle connection event in the namespace
+    namespace.on('connection', () => {
+      namespace.emit('connected', 'hello'); // Emit event to all clients in the namespace
+      logger.info({
+        message: 'New connection to namespace',
+        namespace: namespaceName,
+      });
+    });
+
+    logger.info({
+      message: 'Namespace created successfully',
+      namespace: namespaceName,
+    });
+    return namespace; // Return the created namespace
+  } catch (error) {
+    logger.error({
+      message: 'Error creating namespace',
+      namespace: namespaceName,
+      error,
+    });
+    return null;
+  }
+};
